91f7fc83da128c2a7b4b32905b8e1eb73a9b3f3d
Alexander Barunin <aabzel@yandex.ru>
2020-02-27 01:31:17 +0300

    Create utest.h

---
+++ utest.h
@@ -0,0 +1,22 @@
+#ifndef UTEST_H
+#define UTEST_H
+
+#include <stdio.h>
+#include <string.h>
+
+#include <stdlib.h>
+#include <time.h>
+
+alloc_error_codes_t test_case (void);
+alloc_error_codes_t test_case0 (void);
+alloc_error_codes_t test_case1 (void);
+alloc_error_codes_t test_case2 (void);
+alloc_error_codes_t test_case3 (void);
+alloc_error_codes_t test_case4 (void);
+alloc_error_codes_t test_case5 (void);
+alloc_error_codes_t test_case6 (void);
+alloc_error_codes_t test_case7 (void);
+alloc_error_codes_t test_case8 (void);
+
+#endif /* UTEST_H */
+
