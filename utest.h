34b523a6fd929927180ab48d368050f7614ca5e9
Alexander Barunin <aabzel@yandex.ru>
2020-02-27 01:32:07 +0300

    Update utest.h

--- utest.h
+++ utest.h
@@ -1,11 +1,7 @@
 #ifndef UTEST_H
 #define UTEST_H
 
-#include <stdio.h>
-#include <string.h>
-
-#include <stdlib.h>
-#include <time.h>
+#include "allocator.h"
 
 alloc_error_codes_t test_case (void);
 alloc_error_codes_t test_case0 (void);
