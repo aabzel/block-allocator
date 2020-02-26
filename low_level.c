8af954f8e4a1fddcbdb63530baa8f6c31370483b
Alexander Barunin <aabzel@yandex.ru>
2020-02-27 01:35:31 +0300

    Create low_level.c

---
+++ low_level.c
@@ -0,0 +1,14 @@
+#include "low_level.h"
+
+void disable_interrupts (void) {
+    /*TODO: Add low level code to abolish interrupts
+     * That is needed for the multithreading
+     * */
+}
+
+void enable_interrupts (void) {
+    /*TODO: Add low level code to permit interrupts
+     * That is needed for the multithreading
+     * */
+}
+
