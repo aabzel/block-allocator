410bf5e5033cbd50f42fa9857382efcc61e1318b
Alexander Barunin <aabzel@yandex.ru>
2020-02-27 01:29:23 +0300

    Update allocator.h

--- allocator.h
+++ allocator.h
@@ -1,49 +1,50 @@
-//Author: Alexander Barunin 
-//E-mail: aabzel@yandex.ru
+#ifndef __BLOCK_ALLOCATOR_H
+#define __BLOCK_ALLOCATOR_H
 
-#ifndef __block_allocator_H
-#define __block_allocator_H
+#include <stdint.h>
+
+#define DEBUG_FREE 0
+#define DEBUG_ALLOC 1
 
 #define SIZE_OF_BLOCK 1
-#define SIZE_OF_POOL 10
+#define SIZE_OF_POOL 40
 
-enum alloc_error_codes {
-	ALLOC_OK = 0,
-	ALLOC_ERR_INPUT = 1,
-	ALLOC_ERR_LACK_OF_SPACE = 2,
-	ALLOC_ERR_BLOCK_EXISTS = 3, 
-	ALLOC_BUSY = 4 
-};
+typedef enum {
+    ALLOC_FREE_OK = 0,
+    ALLOC_ERR_INPUT = 1,
+    ALLOC_ERR_LACK_OF_SPACE = 2,
+    ALLOC_ERR_BLOCK_EXISTS = 3,
+    ALLOC_BUSY = 4,
+    FREE_ERR_INPUT_ERROR = 5,
+    FREE_ERROR_NOT_FOUND = 6,
+    FREE_ERROR_UNKNOWN = 7,
+    FREE_ERROR_UNAEBLE_TO_DELETE_TRAILING_BLOCK = 8
+} alloc_error_codes_t;
 
-enum free_error_codes { 
-  FREE_OK = 0,
-  FREE_ERR_INPUT_ERROR = 1,
-  FREE_ERROR_NOT_FOUND = 2,
-  FREE_ERROR_UNKNOWN = 3,
-  FREE_ERROR_UNAEBLE_TO_DELETE_TRAILING_BLOCK = 4
-};
+typedef enum {
+    EMPTY_BLOCK = 0,
+    SINGLE_BLOCK = 1,
+    FIRST_BLOCK_IN_CHAIN = 2,
+    TRAILING_BLOCK_IN_CHAIN = 3,
+    UNDEF_BLOCK = 4
+} block_types_t;
 
-enum block_types {
-  EMPTY_BLOCK = 0,
-  SINGLE_BLOCK = 1, 
-  FIRST_BLOCK_IN_CHAIN = 2,   
-  TRAILING_BLOCK_IN_CHAIN = 3
-};
+typedef struct {
+    // there can be anything: structure, variables, arrays etc...
+    unsigned char data [SIZE_OF_BLOCK];
+} tBlock_t;
 
-struct tBlock {
-	// there can be anything: structure, variables, arrays etc...
-	unsigned char data[SIZE_OF_BLOCK];
-};
+typedef struct {
+    block_types_t blockType;
+    int amountOfSiblingBlocks; /*The number of blocks in the piece into which this block enters*/
+} tPoolStatus_t;
 
-struct tPoolStatus {
-	int blockType; // 
-	int amountOfSiblingBlocks; // The number of blocks in the piece into which this block enters
-};
+void init_allocator (void);
+alloc_error_codes_t allocate_memory_for_blocks (tBlock_t **outBlock, uint32_t ammountOfBlocksToAlloc);
+alloc_error_codes_t free_memory_from_pool (tBlock_t * inBlock);
+char* to_name_str (alloc_error_codes_t errorCode);
 
-int init_allocator();
-int allocate_memory_for_blocks(tBlock **outBlock, int ammountOfBlocksToAlloc);
-int free_memory_from_pool(tBlock *inBlock) ;
 
-void print_pull_status() ;
+void print_pull_status (void);
 
-#endif /* __block_allocator_H */
+#endif /* __BLOCK_ALLOCATOR_H */
